528141627a0f21de09404be5bee59ead0981d56f
mac <mac@macs-iMac.local>
2017-07-07 16:48:35 +0530

    SAVE into Core Data and show on table view and Search data into Core Data is Pending

---
+++ CoreDataTutorais/UserViewController.swift
@@ -0,0 +1,70 @@
+//
+//  UserViewController.swift
+//  CoreDataTutorais
+//
+//  Created by mac on 07/07/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class UserViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tblview_TableView: UITableView!
+    
+    var userArray:[Users] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchData()
+        tblview_TableView.delegate = self
+        tblview_TableView.dataSource = self
+        self.tblview_TableView.reloadData()
+    }
+    
+    
+    //MARK:- Table view Datat Source
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return userArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let name = userArray[indexPath.row]
+        cell.textLabel?.text = name.firstname! + " " + name.lastname!
+        return cell
+    }
+
+    public func numberOfSections(in tableView: UITableView) -> Int{
+        return 1
+    }
+    
+    //MARK:- Delegate method (Delete cell to swip right to left)
+
+     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     
+        if editingStyle == .delete {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let name = userArray[indexPath.row]
+            context.delete(name)
+            fetchData()
+        }
+        tblview_TableView.reloadData()
+     }
+    
+    
+    //MARK:- Creat fucntion for fetching data from CoreData
+    func fetchData(){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            userArray = try context.fetch(Users.fetchRequest())            
+        }
+        catch
+        {
+            print(error)
+        }
+    }
+}
--- CoreDataTutorais/ViewController.swift
+++ CoreDataTutorais/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txt_firstname: UITextField!
 
+    @IBOutlet weak var txt_lastname: UITextField!
+    
+    @IBOutlet weak var txt_Searchuser: UITextField!
+    
+    @IBOutlet weak var lbl_ShowSearchResult: UILabel!
+    
+    var userArray:[Users] = []
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func btn_SaveUser(_ sender: Any) {
+        if txt_firstname.text != "" && txt_lastname.text != ""{
+            let newuser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+            newuser.setValue(txt_firstname.text!, forKey: "firstname")
+            newuser.setValue(txt_lastname.text, forKey: "lastname")
+            
+            //save data into CoreData
+            do
+            {
+                try context.save()
+            }
+            catch
+            {
+                print(error)
+            }
+        }
+        else
+        {
+            print("Please enter firstname and lastname firstly!")
+        }
+        self.txt_lastname.text = ""
+        self.txt_firstname.text = ""
     }
-
-
+    
+    @IBAction func btn_SearchUser(_ sender: Any) {
+    
+    }
+    
+    @IBAction func btn_ShowSaveUsers(_ sender: Any) {
+    }
+    
+    
 }
 
